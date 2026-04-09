143ad4bbf6f413dfe697ea00308bc2ca4dcf69f0
Iliaz Mamedov <93796675+obsoletee@users.noreply.github.com>
2026-04-09 11:51:28 +0300

    fix: avatar size change

--- src/containers/Teachers/TeacherList/TeacherList.tsx
+++ src/containers/Teachers/TeacherList/TeacherList.tsx
@@ -125,13 +125,14 @@ export const TeacherList = ({ handleDeleteTeacher }: TeacherListProps) => {
                 <List.Item.Meta
                   avatar={
                     <Avatar
-    src={
-      <Image
-        src={`${API.url}/teachers/${item._id}/avatar`}
-        fallback={teacherImages.emptyAvatar}
-      />
-    }
-  />
+                    src={
+                      <Image
+                        size={64}
+                        src={`${API.url}/teachers/${item._id}/avatar`}
+                        fallback={teacherImages.emptyAvatar}
+                      />
+                    }
+                  />
                   }
                   title={
                     <Space size={width < 768 ? 'small' : 'large'}>
